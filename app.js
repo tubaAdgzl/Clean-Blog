66835cec66e4f1f40c200c939848833e2851aa8f
Tuba <tadiguzel96@gmail.com>
2021-07-29 19:44:04 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 
+app.set("view engine","ejs")
+
+app.use(express.static("public"))
+
 app.get("/",(req,res) =>{
-    const blog = {
-        id:1,
-        title:"Blog title",
-        description:"Blog description"
-    }
-    res.send(blog)
+    res.render("index")
+})
+
+app.get("/about",(req,res) =>{
+    res.render("about")
+})
+
+app.get("/add",(req,res) =>{
+    res.render("add_post")
 })
 
 const PORT = 3000;
