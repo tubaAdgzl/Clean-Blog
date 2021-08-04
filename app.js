eced511c24d99301a736546d57f4581ed737fa2d
Tuba <tadiguzel96@gmail.com>
2021-08-04 15:04:51 +0300

    post update

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
 const Post = require('./models/Post');
-
+const methodOverride = require('method-override')
 const app = express();
 
 app.use(express.urlencoded({extended:true})) 
 app.use(express.json())
+
+app.use(methodOverride('_method'));
 
 app.set("view engine","ejs")
 
@@ -38,6 +40,22 @@ app.post('/posts', async (req, res) => {
     await Post.create(req.body)           
     res.redirect('/')
   });
+
+app.get('/posts/edit/:id', async (req, res) => {
+    const post = await Post.findOne({ _id: req.params.id });
+    res.render('edit', {
+      post,
+    });
+});
+
+app.put('/posts/:id', async (req,res) =>{
+  const post = await Post.findOne({_id:req.params.id})
+  post.title = req.body.title
+  post.detail = req.body.detail
+  post.save()
+
+  res.redirect(`/posts/${req.params.id}`)
+})
 
 const PORT = 3000;
 
